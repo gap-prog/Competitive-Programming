87f79d17e899e0efadb0dfea21f2c67db8067da9
wcpss-gp <gapradhan@students.wcpss.net>
2022-11-25 16:08:56 -0500

    Updated File Names

---
+++ codeforces/practice/231A_team.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int s = 0;
+	for (int i = 0; i < n; i++) {
+		int p, v, t;
+		cin >> p >> v >> t;
+		if (p + v + t >= 2) {
+			s++;
+		}
+	}
+	cout << s << endl;
+}
+
