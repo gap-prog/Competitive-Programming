1c4ea5f854552f3cd3f9a6c203528fd86cfb9434
wcpss-gp <gapradhan@students.wcpss.net>
2022-11-25 16:44:02 -0500

    Added Solution

---
+++ usaco/practice/2016/dec/bronze/cowsignal.py
@@ -0,0 +1,14 @@
+fin = open('cowsignal.in', 'r')
+m, n, k = map(int, fin.readline().split())
+s = ''
+for i in range(m):
+    ln = fin.readline()
+    nln = ''
+    for j in ln:
+        if j != '\n':
+            nln += j * k
+    s += (nln + '\n') * k
+fin.close()
+fout = open('cowsignal.out', 'w')
+fout.write(s)
+fout.close()
