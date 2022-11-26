121859b4bd64c0a6beeb8e2dfe5638d6e636d5ee
wcpss-gp <gapradhan@students.wcpss.net>
2022-11-26 09:50:20 -0500

    Added Solutions

---
+++ hackerrank/problem_solving/simple_array_sum.py
@@ -0,0 +1,22 @@
+# HackerRank Problem Link: https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def simpleArraySum(ar):
+    return sum(ar)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
