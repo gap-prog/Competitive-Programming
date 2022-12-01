0f39dceaff7b1c25d9478b5e6b0d88a9cc7724eb
gpprog <82526791+gap-prog@users.noreply.github.com>
2022-12-01 11:52:50 -0500

    Updated Solution
    Included problem link.

--- leetcode/223_rectangle_area.cpp
+++ leetcode/223_rectangle_area.cpp
@@ -1,3 +1,5 @@
+// LeetCode Problem Link: https://leetcode.com/problems/rectangle-area/
+
 class Solution {
     public:
         int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
