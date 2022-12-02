0845044113581cdf58c8a9b97e29a4c956703f48
wcpss-gp <gapradhan@students.wcpss.net>
2022-12-01 21:35:49 -0500

    Added Solutions

---
+++ usaco/training/chapter_1/section_1.2/beads.cpp
@@ -0,0 +1,30 @@
+/*
+ID:
+TASK: beads
+LANG: C++
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("beads.in", "r", stdin);
+	freopen("beads.out", "w", stdout);
+	int n, ans;
+	string necklace;
+	cin >> n >> necklace;
+	necklace += necklace;
+	for(int i = 0; i < n; ++i) {
+		char bead = necklace[i];
+		int c = 0;
+		int j = i;
+		for(int k = bead != 'w'; k <= 2; ++k) {
+			for(; j < n + i && (necklace[j] == bead || necklace[j] == 'w'); ++j) {
+				c++;
+			}
+			bead = necklace[j];
+		}
+		ans = max(ans, c);
+	}
+	cout << ans << endl;
+}
