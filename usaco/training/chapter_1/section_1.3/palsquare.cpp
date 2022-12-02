40b8df05fcd479f04c3dd84babeb708631a2561b
wcpss-gp <gapradhan@students.wcpss.net>
2022-12-01 21:41:26 -0500

    USACO/Training - palsquare.cpp

---
+++ usaco/training/chapter_1/section_1.3/palsquare.cpp
@@ -0,0 +1,36 @@
+/*
+ID:
+TASK: palsquare
+LANG: C++
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string toBase(int num, int base) {
+	const string nums = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string result = "";
+	while(num > 0) {
+		int mod = num % base;
+		result += nums[mod];
+		num = (num - mod) / base;
+	}
+	reverse(result.rbegin(), result.rend());
+	return result;
+}
+
+int main() {
+	freopen("palsquare.in", "r", stdin);
+	freopen("palsquare.out", "w", stdout);
+	int b, square;
+	cin >> b;
+	for (int i = 1; i <= 300; ++i) {
+		square = i * i;
+		string convertedSquare = toBase(square, b);
+		string currentNumber = toBase(i, b);
+		string reversed(convertedSquare.rbegin(), convertedSquare.rend());
+		if (reversed == convertedSquare) {
+			cout << currentNumber << " " << convertedSquare << endl;
+		}
+	}
+}
