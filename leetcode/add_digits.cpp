7e0acc66983267fc049e8e7aaeaf68af17fd41c4
gpprog <82526791+gap-prog@users.noreply.github.com>
2022-12-03 20:47:22 -0500

    Create add_digits.cpp
    LeetCode

---
+++ leetcode/add_digits.cpp
@@ -0,0 +1,17 @@
+// LeetCode Problem Link: https://leetcode.com/problems/add-digits/
+
+class Solution {
+    public:
+        int addDigits(int num) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            if (sum < 10) {
+                return sum;
+            } else {
+                return addDigits(sum);
+            }
+        }
+};
