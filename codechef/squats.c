639ccf16e40d6f1e04ad7f54785ef79fbf55ce3a
gpprog <82526791+gap-prog@users.noreply.github.com>
2022-12-06 10:11:31 -0500

    Create squats.c
    CodeChef

---
+++ codechef/squats.c
@@ -0,0 +1,14 @@
+// CodeChef Problem Link: https://www.codechef.com/problems/SQUATS
+
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d\n", &n);
+	while (n--) {
+	    int s;
+	    scanf("%d\n", &s);
+	    printf("%d\n", s * 15);
+	}
+	return 0;
+}
