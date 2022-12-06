619c29da5ce6b8ea68f80d7c6541873b38752ffa
gpprog <82526791+gap-prog@users.noreply.github.com>
2022-12-06 13:39:18 -0500

    Create sequence_equation.cpp
    HackerRank

---
+++ hackerrank/problem_solving/sequence_equation.cpp
@@ -0,0 +1,18 @@
+// HackerRank Problem Link: https://www.hackerrank.com/challenges/permutation-equation/problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int p[n + 1];
+    for (int i = 1; i <= n; ++i) {
+        int x;
+        cin >> x;
+        p[x] = i;
+    }
+    for (int i = 1; i <= n; ++i) {
+        cout << p[p[i]] << endl;
+    }
+}
