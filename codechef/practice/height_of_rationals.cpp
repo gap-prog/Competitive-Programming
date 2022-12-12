c195d4c84ae1f9fb2883874c43b123b917b86fce
gpprog <82526791+gap-prog@users.noreply.github.com>
2022-12-12 09:00:51 -0500

    Create height_of_rationals.cpp
    CodeChef

---
+++ codechef/practice/height_of_rationals.cpp
@@ -0,0 +1,11 @@
+// CodeChef Problem Link: https://www.codechef.com/problems/HEIGHTRATION
+  
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << max(a, b) << endl;
+	return 0;
+}
