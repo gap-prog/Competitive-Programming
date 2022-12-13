ef2c653aa1425d7563b72fbae9a53d61989d2c94
gpprog <82526791+gap-prog@users.noreply.github.com>
2022-12-13 09:33:17 -0500

    Create card_game_for_two.go
    Atcoder

---
+++ atcoder/beginner/card_game_for_two.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  "sort"
+)
+
+func main() {
+  var (n int)
+  fmt.Scan(&n)
+  arr := make([]int, n)
+  for i := 0; i < n; i++ {
+    fmt.Scan(&arr[i])
+  }
+  sort.Sort(sort.Reverse(sort.IntSlice(arr)))
+  a, b := 0, 0
+  for i, v := range arr {
+    if i % 2 == 0 {
+      a += v
+    } else {
+      b += v
+    }
+  }
+  fmt.Println(a - b)
+}
