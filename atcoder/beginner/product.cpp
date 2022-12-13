536fe85e8b6101069d7e4d60753015842dae76c0
gpprog <82526791+gap-prog@users.noreply.github.com>
2022-12-13 09:10:02 -0500

    Create product.cpp
    Atcoder

---
+++ atcoder/beginner/product.cpp
@@ -0,0 +1,10 @@
+// Atcoder Problem Link: https://atcoder.jp/contests/abs/tasks/abc086_a
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int a, b;
+  	cin >> a >> b;
+  	cout << ((a * b % 2 == 0) ? "Even" : "Odd") << endl;
+}
