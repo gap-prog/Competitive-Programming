0096c568fa643b51c2f5f1430f00aca2e0541103
gpprog <82526791+gap-prog@users.noreply.github.com>
2022-12-18 12:32:51 -0500

    Create 281A_word_capitalization.cpp
    CodeForces

---
+++ codeforces/practice/281A_word_capitalization.cpp
@@ -0,0 +1,11 @@
+// CodeForces Problem Link: https://codeforces.com/contest/281/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+  string s;
+  cin >> s;
+  s[0] = toupper(s[0]);
+  cout << s << endl;
+}
