8b201f16ab44cad885272fd97d8a86ba4fef7193
gpprog <82526791+gap-prog@users.noreply.github.com>
2022-12-18 23:00:40 -0500

    Create reach_the_target.cpp
    CodeChef

---
+++ codechef/practice/reach_the_target.cpp
@@ -0,0 +1,15 @@
+// CodeChef Problem Link: https://www.codechef.com/problems/REACHTARGET
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+	    int x, y;
+	    cin >> x >> y;
+	    cout << (x - y) << endl;
+	}
+	return 0;
+}
