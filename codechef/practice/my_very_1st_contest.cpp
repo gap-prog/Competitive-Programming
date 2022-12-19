690084f94397ddbd444bb8391cad7d2ecfa6a651
gpprog <82526791+gap-prog@users.noreply.github.com>
2022-12-18 23:03:49 -0500

    Create my_very_1st_contest.cpp
    CodeChef

---
+++ codechef/practice/my_very_1st_contest.cpp
@@ -0,0 +1,11 @@
+// CodeChef Problem Link: https://www.codechef.com/problems/MY1STCONTEST
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, a, b;
+	cin >> n >> a >> b;
+	cout << (n - a) << ' ' << (n - a - b) << endl;
+	return 0;
+}
