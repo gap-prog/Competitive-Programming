9f696036546686e1adfbac82b9f5567912038a34
gpprog <82526791+gap-prog@users.noreply.github.com>
2022-12-25 17:41:42 -0500

    Create chef_and_brain_speed.cpp
    CodeChef

---
+++ solutions/codechef/practice/chef_and_brain_speed.cpp
@@ -0,0 +1,11 @@
+// CodeChef Problem Link: https://www.codechef.com/problems/CBSPEED
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	cout << ((y > x) ? "YES" : "NO");
+	return 0;
+}
