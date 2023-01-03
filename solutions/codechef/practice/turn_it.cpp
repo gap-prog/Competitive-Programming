1e57ec887b8510d5836e70ddb502de91c9fae221
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-03 09:22:15 -0500

    Create turn_it.cpp
    CodeChef

---
+++ solutions/codechef/practice/turn_it.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define spc " "
+#define nl '\n'
+#define sz(x) ((int) x.size())
+
+void solve() {
+        float U, V, A, S;
+        cin >> U >> V >> A >> S;
+        float v = sqrt((U * U) - (2 * A * S));
+        cout << ((V < v) ? "NO" : "YES") << endl;
+}
+
+int main() {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        int t;
+        cin >> t;
+        while (t--) {
+                solve();
+        }
+        return 0;
+}
