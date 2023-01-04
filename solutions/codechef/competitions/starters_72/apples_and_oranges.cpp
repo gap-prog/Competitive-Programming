371a7448f8e28020aaabe8407643bb1afb9eef06
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-04 12:35:13 -0500

    Create apples_and_oranges.cpp
    CodeChef

---
+++ solutions/codechef/competitions/starters_72/apples_and_oranges.cpp
@@ -0,0 +1,15 @@
+// CodeChef Problem Link: https://www.codechef.com/START72C/problems/APPLEORANGE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+	    int n, m;
+	    cin >> n >> m;
+	    cout << gcd(n, m) << endl;
+	}
+	return 0;
+}
