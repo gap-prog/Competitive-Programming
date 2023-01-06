2b0f38199f322ff50c77bb5ca9605f578a0d17a8
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-06 08:03:49 -0500

    Create solving_for_carrots.cpp
    Open Kattis

---
+++ solutions/open_kattis/solving_for_carrots.cpp
@@ -0,0 +1,18 @@
+// Open Kattis Problem Link: https://open.kattis.com/contests/nxmyxh/problems/carrots
+
+/*
+Details
+-------
+STRATEGY: output the number of problems solved
+I/O: terminal/stdin
+APPROACH: realize that the input assigned to variable `p` is the solution
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, p;
+    cin >> n >> p;
+    cout << p << endl;
+}
