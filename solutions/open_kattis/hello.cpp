f2abaa574a59da284fd8475a85547dff3d2bfa27
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-06 08:01:46 -0500

    Create hello.cpp
    Open Kattis

---
+++ solutions/open_kattis/hello.cpp
@@ -0,0 +1,16 @@
+// Open Kattis Problem Link: https://open.kattis.com/contests/nxmyxh/problems/hello
+
+/*
+Details
+-------
+STRATEGY: output a string
+I/O: terminal/stdin
+APPROACH: output a string
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+}
