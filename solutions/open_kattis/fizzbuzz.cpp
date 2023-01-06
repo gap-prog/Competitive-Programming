e139d055df83ba49ca302220c0d4b1b638c8cadf
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-06 14:12:41 -0500

    Create fizzbuzz.cpp
    Open Kattis

---
+++ solutions/open_kattis/fizzbuzz.cpp
@@ -0,0 +1,20 @@
+// Open Kattis Problem Link: https://open.kattis.com/problems/fizzbuzz
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y, n;
+    cin >> x >> y >> n;
+    for (int i = 1; i < n + 1; ++i) {
+        if (i % x == 0 && i % y == 0) {
+            cout << "FizzBuzz" << endl;
+        } else if (i % x == 0) {
+            cout << "Fizz" << endl;
+        } else if (i % y == 0) {
+            cout << "Buzz" << endl;
+        } else {
+            cout << i << endl;
+        }
+    }
+}
