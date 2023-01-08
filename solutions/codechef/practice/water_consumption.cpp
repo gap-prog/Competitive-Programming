e1c3f93bdfda2f5bf7ee7ed8748c7ae49af358e5
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-08 18:03:41 -0500

    Create water_consumption.cpp
    CodeChef

---
+++ solutions/codechef/practice/water_consumption.cpp
@@ -0,0 +1,15 @@
+// CodeChef Problem Link: https://www.codechef.com/problems/WATERCONS
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+	    int x;
+	    cin >> x;
+	    cout << ((x >= 2000) ? "YES" : "NO") << endl;
+	}
+	return 0;
+}
