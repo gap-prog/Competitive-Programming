6593ea1155255682925e374c5eb5b13e8fd32707
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-07 21:14:05 -0500

    Create squats.cpp
    CodeChef

---
+++ solutions/codechef/practice/squats.cpp
@@ -0,0 +1,15 @@
+// CodeChef Problem Link: https://www.codechef.com/problems/SQUATS
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+	    int x;
+	    cin >> x;
+	    cout << x * 15 << endl;
+	}
+	return 0;
+}
