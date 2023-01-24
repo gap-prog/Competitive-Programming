83615ceb4abaa150e6db10de51888abc81388d17
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-24 11:21:48 -0500

    Create loneTeen.java
    coding_bat

---
+++ solutions/coding_bat/warm-up-1/loneTeen.java
@@ -0,0 +1,11 @@
+// CodingBat Problem Link: https://codingbat.com/prob/p165701
+
+public boolean loneTeen(int a, int b) {
+  if((a>=13&&a<=19)&&(b<13||b>19)){
+    return true;
+  }else if((b>=13&&b<=19)&&(a<13||a>19)){
+    return true;
+  }else{
+    return false;
+  }
+}
