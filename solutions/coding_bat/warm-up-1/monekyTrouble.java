c9b0b2af89af38d94ba799aab75f81d13a2903fc
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-24 10:24:57 -0500

    Create monekyTrouble.java
    coding_bat

---
+++ solutions/coding_bat/warm-up-1/monekyTrouble.java
@@ -0,0 +1,9 @@
+// CodingBat Problem Link: https://codingbat.com/prob/p181646
+
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+  if((aSmile&&bSmile)||(!aSmile&&!bSmile)){
+    return true;
+  }else{
+    return false;
+  }
+}
