073ffc8a9b7251b4c14e21b570ebae5369f70123
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-24 11:19:08 -0500

    Create hasTeen.java
    coding_bat

---
+++ solutions/coding_bat/warm-up-1/hasTeen.java
@@ -0,0 +1,9 @@
+// CodingBat Problem Link: https://codingbat.com/prob/p178986
+
+public boolean hasTeen(int a, int b, int c) {
+  if((a>=13&&a<=19)||(b>=13&&b<=19)||(c>=13&&c<=19)){
+    return true;
+  }else{
+    return false;
+  }
+}
