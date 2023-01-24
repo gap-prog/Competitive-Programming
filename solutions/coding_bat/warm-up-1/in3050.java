d2f9a487a5d3dac940aaeea6b0f7e7a5213ccf66
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-24 11:38:40 -0500

    Create in3050.java
    coding_bat

---
+++ solutions/coding_bat/warm-up-1/in3050.java
@@ -0,0 +1,9 @@
+// CodingBat Problem Link: https://codingbat.com/prob/p132134
+
+public boolean in3050(int a, int b) {
+  if(((a>=30&&a<=40)&&(b>=30&&b<=40))||((a>=40&&a<=50)&&(b>=40&&b<=50))){
+    return true;
+  }else{
+    return false;
+  }
+}
