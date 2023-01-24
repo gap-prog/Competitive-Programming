49c96506508ac47e272718fe6b5522ada5a5c49f
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-24 10:38:07 -0500

    Create makes10.java
    coding_bat

---
+++ solutions/coding_bat/warm-up-1/makes10.java
@@ -0,0 +1,9 @@
+// CodingBat Problem Link: https://codingbat.com/prob/p182873
+
+public boolean makes10(int a, int b) {
+  if((a+b==10)||(a==10||b==10)){
+    return true;
+  }else{
+    return false;
+  }
+}
