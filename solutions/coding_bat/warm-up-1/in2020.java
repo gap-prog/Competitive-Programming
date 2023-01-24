f52bc36fce345963be5af727fdb10cbdf73dbafa
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-24 11:16:32 -0500

    Create in2020.java
    coding_bat

---
+++ solutions/coding_bat/warm-up-1/in2020.java
@@ -0,0 +1,9 @@
+// CodingBat Problem Link: https://codingbat.com/prob/p144535
+
+public boolean in1020(int a, int b) {
+  if((a>=10&&a<=20)||(b>=10&&b<=20)){
+    return true;
+  }else{
+    return false;
+  }
+}
