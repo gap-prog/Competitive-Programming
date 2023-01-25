4c1e79f3d35a52a81f8659e170df1bf2eff0e754
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-01-25 09:20:31 -0500

    Create stringTimes.java
    coding_bat

---
+++ solutions/coding_bat/warm-up-1/stringTimes.java
@@ -0,0 +1,9 @@
+// CodingBat Problem Link: https://codingbat.com/prob/p142270
+
+public String stringTimes(String str, int n) {
+  String finalString="";
+  for(int i=0;i<n;++i){
+    finalString+=str;
+  }
+  return finalString;
+}
