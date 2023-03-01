c55cd61d35db52e479f883b28a8ce9598e56665d
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-03-01 12:57:41 -0500

    Create 1791A_codeforces_checking.cpp
    CodeForces

---
+++ solutions/codeforces/practice/1791A_codeforces_checking.cpp
@@ -0,0 +1,21 @@
+// CodeForces Problem Link: 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        char c;
+        cin >> c;
+        if (c == 'c' || c == 'o' || c == 'd' || c == 'e' || c == 'f' || c == 'r' || c == 's') {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
