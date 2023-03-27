f0932b4a04e5e81d5cb23af55d9a3cbf25481cab
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-03-26 21:12:16 -0400

    Update life_the_universe_and_everything.cpp
    HackerEarth

--- solutions/hacker_earth/life_the_universe_and_everything.cpp
+++ solutions/hacker_earth/life_the_universe_and_everything.cpp
@@ -1,3 +1,5 @@
+// HackerEarth Problem Link: https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/life-the-universe-and-everything/
+
 #include <bits/stdc++.h>
 using namespace std;
 
