749371e5d403eec4f433adaebcffea634757d124
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-03-26 21:11:39 -0400

    Update zoos.cpp
    HackerEarth

--- solutions/hacker_earth/zoos.cpp
+++ solutions/hacker_earth/zoos.cpp
@@ -1,3 +1,5 @@
+// HackerEarth Problem Link: https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/is-zoo-f6f309e7/
+
 #include <bits/stdc++.h>
 using namespace std;
 
