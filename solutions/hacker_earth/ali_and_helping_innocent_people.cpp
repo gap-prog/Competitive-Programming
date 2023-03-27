641e6dd4e0e80a9bd65e16425f711accb2ca09fd
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-03-26 21:12:44 -0400

    Update ali_and_helping_innocent_people.cpp
    HackerEarth

--- solutions/hacker_earth/ali_and_helping_innocent_people.cpp
+++ solutions/hacker_earth/ali_and_helping_innocent_people.cpp
@@ -1,3 +1,5 @@
+// HackerEarth Problem Link: https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/cartag-948c2b02/
+
 #include <bits/stdc++.h>
 using namespace std;
 
