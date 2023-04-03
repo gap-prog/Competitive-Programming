fdbcdf3d3bef7e8dbfc5a793a7edf54d9c978004
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-04-03 19:20:54 -0400

    Create calculate_the_power.py

---
+++ solutions/hacker_earth/practice/calculate_the_power.py
@@ -0,0 +1,4 @@
+# HackerEarth Problem Link: https://www.hackerearth.com/practice/math/number-theory/basic-number-theory-1/practice-problems/algorithm/calculate-the-power/
+
+a = input().split()
+print(pow(int(a[0]), int(a[1]), 1000000007))
