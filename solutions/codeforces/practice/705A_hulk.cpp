08603154f9df767c593903670d9ab9e79ff24300
wcpss-gp <gapradhan@students.wcpss.net>
2023-05-14 13:29:22 -0400

    705A_hulk.cpp

---
+++ solutions/codeforces/practice/705A_hulk.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; ++i) {
+		if (i == n) {
+			if (i % 2 == 0) cout << "I love it ";
+			else cout << "I hate it ";
+		} else {
+			if (i % 2 == 0) cout << "I love that ";
+			else cout << "I hate that ";
+		}
+	}
+	return 0;
+}
+
