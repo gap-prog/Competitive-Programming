8fef120be291623f77aa2b87532b9e9a85ea9b8c
wcpss-gp <gapradhan@students.wcpss.net>
2023-05-19 17:29:26 -0400

    1833A_musical_puzzle.cpp

---
+++ solutions/codeforces/practice/1833A_musical_puzzle.cpp
@@ -0,0 +1,15 @@
+// CodeForces Problem Link: https://codeforces.com/contest/1828/my
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		for (int i = 0; i < n; ++i) cout << 2 * i << ' ';
+	}
+	return 0;
+}
