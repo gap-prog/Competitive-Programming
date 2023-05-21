62ce075403d4d6506712be9e94e9f1edee747f2a
wcpss-gp <gapradhan@students.wcpss.net>
2023-05-21 12:31:16 -0400

    AtCoder

---
+++ solutions/atcoder/beginner_contest/302/attack.cpp
@@ -0,0 +1,12 @@
+// AtCoder Problem Link: https://atcoder.jp/contests/abc302/tasks/abc302_a
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+	ll a, b;
+	cin >> a >> b;
+	cout << (a + b - 1) / b << '\n';
+	return 0;
+}
