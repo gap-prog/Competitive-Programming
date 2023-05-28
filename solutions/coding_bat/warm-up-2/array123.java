6c80b6e1baee422ffa6e69406498c198aea4a5f2
wcpss-gp <gapradhan@students.wcpss.net>
2023-05-28 18:12:57 -0400

    Update array123.java

--- solutions/coding_bat/warm-up-2/array123.java
+++ solutions/coding_bat/warm-up-2/array123.java
@@ -1,3 +1,5 @@
+// CodingBat Problem Link: https://codingbat.com/java/Warmup-2
+
 public boolean array123(int[] nums){
   for (int i=0;i<nums.length-2;++i){
 	  if (nums[i]==1&&nums[i+1]==2&&nums[i+2]==3){
