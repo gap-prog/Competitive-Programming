e59ae0618d7ebfccea2915118f30fa135119fb36
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-06-02 20:02:21 -0400

    Create 28_find_the_index_of_the_first_occurrence_in_a_string.py

---
+++ solutions/leetcode/practice/28_find_the_index_of_the_first_occurrence_in_a_string.py
@@ -0,0 +1,5 @@
+# LeetCodeProblem Link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
