9535e09cfb69e790186145ea1cd4e348171f1730
wcpss-gp <gapradhan@students.wcpss.net>
2023-06-03 13:57:49 -0400

    Create 456A_laptops.cpp

---
+++ solutions/codeforces/practice/456A_laptops.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	vector<pair<int, int>> v(n);
+	for (pair<int, int> &i: v) cin >> i.first >> i.second;
+	sort(v.begin(), v.end());
+	bool found = false;
+	for (int i = 0; i < n - 1; ++i) {
+		if (v[i].second > v[i + 1].second) {
+			found = true;
+			break;
+		}
+	}
+	cout << (found ? "Happy Alex" : "Poor Alex") << '\n';
+	return 0;
+}
