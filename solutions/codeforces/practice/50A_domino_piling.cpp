7e83eb1254f96bc95551f33b63103d3e934d8de5
wcpss-gp <gapradhan@students.wcpss.net>
2023-06-14 17:13:48 -0400

    Create 50A_domino_piling.cpp
    CodeForces

---
+++ solutions/codeforces/practice/50A_domino_piling.cpp
@@ -0,0 +1,10 @@
+// CodeForces Problem Link: https://codeforces.com/problemset/problem/50/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m; cin >> n >> m;
+    cout << (n * m) / 2 << '\n';
+    return 0;
+}
