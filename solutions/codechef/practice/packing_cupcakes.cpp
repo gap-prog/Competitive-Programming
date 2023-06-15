b3e777f0633d373fbb165af3759b6f47415a54c4
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-06-15 10:42:21 -0400

    Create packing_cupcakes.cpp
    CodeChef

---
+++ solutions/codechef/practice/packing_cupcakes.cpp
@@ -0,0 +1,15 @@
+// CodeChef Problem Link: https://www.codechef.com/problems/MUFFINS3
+
+#include <iostream>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    cout << n / 2 + 1 << '\n';
+}
+
+int main() {
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
