6a33188dcd04fcb557dbcd84f865ea2068878f54
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-06-27 15:56:01 -0400

    Create 199A_hexadecimals_theorem.cpp
    CodeForces

---
+++ solutions/codeforces/practice/199A_hexadecimals_theorem.cpp
@@ -0,0 +1,10 @@
+// CodeForces Problem Link: https://codeforces.com/problemset/problem/199/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n; cin >> n;
+  cout << "0 0 " << n << '\n';
+  return 0;
+}
