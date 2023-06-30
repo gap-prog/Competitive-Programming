241fb6e0848c7b2272b314dcdc2e7986a2fedda3
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-06-30 15:13:21 -0400

    Create mowing_the_field.cpp
    USACO

--- solutions/usaco/practice/2016/jan/bronze/mowing_the_field.cpp
+++ solutions/usaco/practice/2016/jan/bronze/mowing_the_field.cpp
@@ -3,40 +3,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int curr = 0, x = 1000, y = 1000, ans = INT_MAX;
+vector<vector<int>> v(2000, vector<int>(2000, -1));
+
 int main() {
-        int n;
-        cin >> n;
-        vector<vector<int>> v(2001, vector<int>(2001));
-        for (int i = 0; i < v.size(); ++i) {
-                for (int j = 0; j < v[i].size(); ++j) {
-                        v[i][j] = -1;
-                }
+    ifstream fin("mowing.in");
+    ofstream fout("mowing.out");
+    v[x][y] = 0;
+    int n; fin >> n;
+    for (int i = 0; i < n; ++i) {
+        char d; fin >> d;
+        int s; fin >> s;
+        for (int j = 1; j <= s; ++j) {
+            if (d == 'N') x--;
+            else if (d == 'S') x++;
+            else if (d == 'E') y++;
+            else if (d == 'W') y--;
+            curr++;
+            if (v[x][y] >= 0) ans = min(ans, curr - v[x][y]);
+            v[x][y] = curr;
         }
-        int curr_x = 1000, curr_y = 1000, curr_t = 0, ans = 1001;
-        v[curr_x][curr_y] = 0;
-        for (int i = 0; i < n; ++i) {
-                char dir;
-                int dir_x = 0, dir_y = 0, steps;
-                cin >> dir >> steps;
-                if (dir == 'N') {
-                        dir_x = -1;
-                } else if (dir == 'S') {
-                        dir_x = 1;
-                } else if (dir == 'W') {
-                        dir_y = -1;
-                } else {
-                        dir_y = 1;
-                }
-                for (int j = 1; j <= steps; ++j) {
-                        curr_x += dir_x;
-                        curr_y += dir_y;
-                        curr_t++;
-                        if (v[curr_x][curr_y] >= 0 && curr_t - v[curr_x][curr_y] < ans) {
-                                ans = curr_t - v[curr_x][curr_y];
-                        }
-                        v[curr_x][curr_y] = curr_t;
-                }
-        }
-        cout << ((ans == 1001) ? -1 : ans) << endl;
-        return 0;
+    }
+    fout << (ans == INT_MAX ? -1 : ans) << '\n';
+    return 0;
 }
