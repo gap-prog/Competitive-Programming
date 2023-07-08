f29eae370e6bc61ea73c9b7ffc84a35b9b8d349e
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-07-08 10:50:02 -0400

    Create guess_the_animal.cpp
    USACO

--- solutions/usaco/practice/2019/jan/bronze/guess_the_animal.cpp
+++ solutions/usaco/practice/2019/jan/bronze/guess_the_animal.cpp
@@ -4,30 +4,30 @@
 using namespace std;
 
 int main() {
-	freopen("guess.in", "r", stdin);
-	freopen("guess.out", "w", stdout);
-	int n;
-	cin >> n;
-	vector<set<string>> v(n);
-	for (int i = 0; i < n; ++i) {
-	    string animal;
-	    int k;
-	    cin >> animal >> k;
-	    for (int j = 0; j < k; ++j) {
-	        string c;
-	        cin >> c;
-	        v[i].insert(c);
-	    }
-	}
-	int num_common = 0;
-	for (int i = 0; i < n; ++i) {
-		for (int j = i + 1; j < n; ++j) {
-			set<string> intersect;
-			set_intersection(v[i].begin(), v[i].end(), v[j].begin(), v[j].end(), std::inserter(intersect, intersect.begin()));
-			if (intersect.size() > num_common) {
-				num_common = intersect.size();
-			}
-		}
-	}
-	cout << num_common + 1 << endl;
+  ifstream fin("guess.in");
+  ofstream fout("guess.out");
+  int n; fin >> n;
+  vector<set<string>> animals(n);
+  for (set<string> &i: animals) {
+    string animal; fin >> animal;
+    int k; fin >> k;
+    while (k--) {
+      string characteristic; fin >> characteristic;
+      i.insert(characteristic);
+    }
+  }
+  int MAX = INT_MIN;
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      set<string> intersection;
+      set_intersection(
+        begin(animals[i]), end(animals[i]),
+        begin(animals[j]), end(animals[j]),
+        inserter(intersection, begin(intersection))
+      );
+      MAX = max(MAX, (int) intersection.size() + 1);
+    }
+  }
+  fout << MAX << '\n';
+  return 0;
 }
