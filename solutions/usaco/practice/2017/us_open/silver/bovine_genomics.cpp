89a012643b900d0630dcacccadc9c1c385856950
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-07-10 18:01:06 -0400

    Create bovine_genomics.cpp
    USACO

--- solutions/usaco/practice/2017/us_open/silver/bovine_genomics.cpp
+++ solutions/usaco/practice/2017/us_open/silver/bovine_genomics.cpp
@@ -3,53 +3,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+set<string> st;
+string str = "...";
+
 int main() {
-        freopen("cownomics.in", "r", stdin);
-        freopen("cownomics.out", "w", stdout);
-        int n, m, ans = 0;
-        cin >> n >> m;
-        vector<vector<int>> spotty(n, vector<int>(m));
-        vector<vector<int>> plain(n, vector<int>(m));
-        for (int i = 0; i < n; ++i) {
-                string s;
-                cin >> s;
-                for (int j = 0; j < m; ++j) {
-                        if (s[j] == 'A') spotty[i][j] = 0;
-                        else if (s[j] == 'T') spotty[i][j] = 1;
-                        else if (s[j] == 'C') spotty[i][j] = 2;
-                        else spotty[i][j] = 3;
-                }
+  ifstream fin("cownomics.in");
+  ofstream fout("cownomics.out");
+  int n, m; fin >> n >> m;
+  vector<string> spotted(n), plain(n);
+  for (string &i: spotted) fin >> i;
+  for (string &i: plain) fin >> i;
+  int ans = 0;
+  for (int i = 0; i < m; ++i) {
+    for (int j = i + 1; j < m; ++j) {
+      for (int k = j + 1; k < m; ++k) {
+        st.clear();
+        bool check = true;
+        for (int x = 0; x < n; ++x) {
+          str[0] = spotted[x][i];
+          str[1] = spotted[x][j];
+          str[2] = spotted[x][k];
+          st.insert(str);
         }
-        for (int i = 0; i < n; ++i) {
-                string s;
-                cin >> s;
-                for (int j = 0; j < m; ++j) {
-                        if (s[j] == 'A') plain[i][j] = 0;
-                        else if (s[j] == 'T') plain[i][j] = 1;
-                        else if (s[j] == 'C') plain[i][j] = 2;
-                        else plain[i][j] = 3;
-                }
+        for (int x = 0; x < n; ++x) {
+          str[0] = plain[x][i];
+          str[1] = plain[x][j];
+          str[2] = plain[x][k];
+          if (st.count(str)) {
+            check = false;
+            break;
+          }
         }
-        for (int i = 0; i < m - 2; ++i) {
-                for (int j = i + 1; j < m - 1; ++j) {
-                        for (int k = j + 1; k < m; ++k) {
-                                vector<bool> spotted(64);
-                                bool check = true;
-                                for (int l = 0; l < n; ++l) {
-                                        int ret = spotty[l][i] * 16 + spotty[l][j] * 4 + spotty[l][k];
-                                        spotted[ret] = true;
-                                }
-                                for (int l = 0; l < n; ++l) {
-                                        int ret = plain[l][i] * 16 + plain[l][j] * 4 + plain[l][k];
-                                        if (spotted[ret]) {
-                                                check = false;
-                                                break;
-                                        }
-                                }
-                                ans += check;
-                        }
-                }
-        }
-        cout << ans << endl;
-        return 0;
+        ans += check;
+      }
+    }
+  }
+  fout << ans << '\n';
+  return 0;
 }
