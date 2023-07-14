3c0d4f20dd2a289e8f980046273bd800d659a264
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-07-14 14:57:37 -0400

    Create moocast.cpp
    USACO

--- solutions/usaco/practice/2016/dec/silver/moocast.cpp
+++ solutions/usaco/practice/2016/dec/silver/moocast.cpp
@@ -3,37 +3,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dfs(int cow, vector<bool> &visited, vector<vector<bool>> &transmit) {
-	if (visited[cow]) return 0;
-	visited[cow] = true;
-	int ret = 1;
-	for (int i = 0; i < transmit[cow].size(); ++i) {
-		if (transmit[cow][i]) {
-			ret += dfs(i, visited, transmit);
-		}
-	}
-	return ret;
+const int N = 25000;
+
+int n;
+
+int dfs(int node, vector<bool> &visited, vector<vector<int>> &adj) {
+  visited[node] = true;
+  int res = 1;
+  for (int i = 0; i < n; ++i) if (!visited[i] && adj[node][i]) res += dfs(i, visited, adj);
+  return res;
 }
 
 int main() {
-	freopen("moocast.in", "r", stdin);
-	freopen("moocast.out", "w", stdout);
-	int n;
-	cin >> n;
-	vector<int> x(n), y(n), p(n);
-	for (int i = 0; i < n; ++i) cin >> x[i] >> y[i] >> p[i];
-	vector<vector<bool>> transmit(n, vector<bool>(n));
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			int dist_squared = (x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]);
-			if (dist_squared <= p[i] * p[i]) transmit[i][j] = true;
-		}
-	}
-	int ans = 1;
-	for (int i = 0; i < n; ++i) {
-		vector<bool> visited(n);
-		ans = max(ans, dfs(i, visited, transmit));
-	}
-	cout << ans << '\n';
-	return 0;
+  ifstream fin("moocast.in");
+  ofstream fout("moocast.out");
+  fin >> n;
+  vector<array<int, 3>> cow(n);
+  for (array<int, 3> &i: cow) fin >> i[0] >> i[1] >> i[2];
+  vector<vector<int>> adj(n, vector<int>(n));
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      int x = abs(cow[i][0] - cow[j][0]), y = abs(cow[i][1] - cow[j][1]);
+      int dist = x * x + y * y;
+      adj[i][j] = dist <= cow[i][2] * cow[i][2];
+    }
+  }
+  int ans = 0;
+  for (int i = 0; i < n; ++i) {
+    vector<bool> visited(n);
+    ans = max(ans, dfs(i, visited, adj));
+  }
+  fout << ans << '\n';
+  return 0;
 }
