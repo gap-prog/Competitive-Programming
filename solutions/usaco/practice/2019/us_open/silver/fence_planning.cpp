41a23c727e35ef864259543e17fbd2dd4c914b4a
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-07-20 11:38:04 -0400

    Create fence_planning.cpp
    USACO

--- solutions/usaco/practice/2019/us_open/silver/fence_planning.cpp
+++ solutions/usaco/practice/2019/us_open/silver/fence_planning.cpp
@@ -1,52 +1,49 @@
+// USACO Problem Link: http://www.usaco.org/index.php?page=viewproblem2&cpid=944
+
 #include <bits/stdc++.h>
 using namespace std;
+#define sz(x) (int) x.size()
 
-struct Cow {
-	int x, y;
-	bool visited;
-	vector<int> adj;
-};
+const int N = 1e5 + 1;
 
-void dfs(int cow, vector<Cow> &cows, vector<int> &network) {
-	cows[cow].visited = true;
-	network.push_back(cow);
-	for (int &i: cows[cow].adj) if (!cows[i].visited) dfs(i, cows, network);
+vector<bool> visited(N);
+vector<vector<int>> components;
+vector<int> x(N), y(N), adj[N], current;
+
+void dfs(int node) {
+  visited[node] = true;
+  current.emplace_back(node);
+  for (int &i: adj[node]) if (!visited[i]) dfs(i);
 }
 
 int main() {
-	freopen("fenceplan.in", "r", stdin);
-	freopen("fenceplan.out", "w", stdout);
-	int n, m;
-	cin >> n >> m;
-	vector<Cow> cows(n);
-	for (Cow &i: cows) {
-		cin >> i.x >> i.y;
-		i.visited = false;
-	}
-	for (int i = 0; i < m; ++i) {
-		int a, b;
-		cin >> a >> b;
-		a--, b--;
-		cows[a].adj.push_back(b);
-		cows[b].adj.push_back(a);
-	}
-	vector<vector<int>> moonet;
-	for (int i = 0; i < n; ++i) {
-		if (!cows[i].visited) {
-			vector<int> network;
-			dfs(i, cows, network);
-			moonet.push_back(network);
-		}
-	}
-	int ans = INT_MAX;
-	for (vector<int> &i: moonet) {
-		int minx = INT_MAX, maxx = 0, miny = INT_MAX, maxy = 0;
-		for (int &j: i) {
-			minx = min(minx, cows[j].x), maxx = max(maxx, cows[j].x);
-			miny = min(miny, cows[j].y), maxy = max(maxy, cows[j].y);
-		}
-		ans = min(ans, 2 * (maxx - minx) + 2 * (maxy - miny));
-	}
-	cout << ans << endl;
-	return 0;
+  ifstream fin("fenceplan.in");
+  ofstream fout("fenceplan.out");
+  int n, m; fin >> n >> m;
+  for (int i = 1; i <= n; ++i) fin >> x[i] >> y[i];
+  for (int i = 0; i < m; ++i) {
+    int a, b; fin >> a >> b;
+    adj[a].emplace_back(b);
+    adj[b].emplace_back(a);
+  }
+  for (int i = 1; i <= n; ++i) {
+    if (!visited[i]) {
+      current.clear();
+      dfs(i);
+      components.emplace_back(current);
+    }
+  }
+  int ans = INT_MAX;
+  for (vector<int> &i: components) {
+    int minx = INT_MAX, maxx = 0, miny = INT_MAX, maxy = 0;
+    for (int j = 0; j < sz(i); ++j) {
+      minx = min(minx, x[i[j]]);
+      maxx = max(maxx, x[i[j]]);
+      miny = min(miny, y[i[j]]);
+      maxy = max(maxy, y[i[j]]);
+    }
+    ans = min(ans, 2 * (maxx - minx) + 2 * (maxy - miny));
+  }
+  fout << ans << '\n';
+  return 0;
 }
