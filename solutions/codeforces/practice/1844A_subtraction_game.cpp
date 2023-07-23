1df789d73662b36bee22b4a598a9f0c70fa3e44d
gpprog <82526791+gap-prog@users.noreply.github.com>
2023-07-22 20:28:01 -0400

    Create 1844A_subtraction_game.cpp
    CodeForces

---
+++ solutions/codeforces/practice/1844A_subtraction_game.cpp
@@ -0,0 +1,16 @@
+// CodeForces Problem Link: https://codeforces.com/problemset/problem/1844/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  int a, b; cin >> a >> b;
+  cout << a + b << '\n';
+}
+
+int main() {
+  int t; cin >> t;
+  while (t--) solve();
+
+  return 0;
+}
